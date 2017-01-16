578e95613b7a885a7feb9504f667c5ec8c5f1178
Scott Dalwood <ubuxr@prod.atonet.gov.au>
2017-01-16 16:56:34 +1100

    Update global.R to process 'measures' as per extraction files

--- global.R
+++ global.R
@@ -43,49 +43,49 @@ ts_pl <- ts(ts_pl$R12, start = c(ts_strt_yr, ts_strt_mth), frequency = 12)
 
 # Ongoing hc 
 ts_ongHC <- read_csv("data/ts_ongHC.csv")
-ts_ongHC <- ts(ts_ongHC$Headcount, start = c(2007, 7), frequency = 12) 
+ts_ongHC <- ts(ts_ongHC$Measure, start = c(2007, 7), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Non-ongoing hc 
 ts_nonHC <- read_csv("data/ts_nonHC.csv")
-ts_nonHC <- ts(ts_nonHC$Headcount, start = c(2007, 7), frequency = 12)
+ts_nonHC <- ts(ts_nonHC$Measure, start = c(2007, 7), frequency = 12)
 
 #__________________________________________________________________________________________________#
 
 # Casual hc 
 ts_casHC <- read_csv("data/ts_casHC.csv")
-ts_casHC <- ts(ts_casHC$Headcount, start = c(2007, 7), frequency = 12) 
+ts_casHC <- ts(ts_casHC$Measure, start = c(2007, 7), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Total hc 
 ts_totHC <- read_csv("data/ts_totHC.csv")
-ts_totHC <- ts(ts_totHC$Headcount, start = c(2007, 7), frequency = 12) 
+ts_totHC <- ts(ts_totHC$Measure, start = c(2007, 7), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Ongoing fte
 ts_ongFTE <- read_csv("data/ts_ongFTE.csv")
-ts_ongFTE <- ts(ts_ongFTE$Paid_FTE, start = c(2014, 3), frequency = 12) 
+ts_ongFTE <- ts(ts_ongFTE$Measure, start = c(2014, 3), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Non-ongoing fte
 ts_nonFTE <- read_csv("data/ts_nonFTE.csv")
-ts_nonFTE <- ts(ts_nonFTE$Paid_FTE, start = c(2014, 3), frequency = 12) 
+ts_nonFTE <- ts(ts_nonFTE$Measure, start = c(2014, 3), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Casual fte
 ts_casFTE <- read_csv("data/ts_casFTE.csv")
-ts_casFTE <- ts(ts_casFTE$Paid_FTE, start = c(2014, 3), frequency = 12) 
+ts_casFTE <- ts(ts_casFTE$Measure, start = c(2014, 3), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
 # Total fte
 ts_totFTE <- read_csv("data/ts_totFTE.csv")
-ts_totFTE <- ts(ts_totFTE$Paid_FTE, start = c(2014, 3), frequency = 12) 
+ts_totFTE <- ts(ts_totFTE$Measure, start = c(2014, 3), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
@@ -109,19 +109,19 @@ ts_ong <- ts(ts_ong$R12_Ongoing_Rate, start = c(2008, 7), frequency = 12)
 
 # Age 
 ts_age <- read_csv("data/ts_age.csv")
-ts_age <- ts(ts_age$Average_Age, start = c(2007, 7), frequency = 12)
+ts_age <- ts(ts_age$Measure, start = c(2007, 7), frequency = 12)
 
 #__________________________________________________________________________________________________#
 
 # AtoTnr
 ts_atoTnr <- read_csv("data/ts_atoTnr.csv")
-ts_atoTnr <- ts(ts_atoTnr$Average_ATO_Tenure, start = c(2007, 7), frequency = 12)
+ts_atoTnr <- ts(ts_atoTnr$Measure, start = c(2007, 7), frequency = 12)
 
 #__________________________________________________________________________________________________#
 
 # ApsTnr 
 ts_apsTnr <- read_csv("data/ts_apsTnr.csv")
-ts_apsTnr <- ts(ts_apsTnr$Average_APS_Tenure, start = c(2007, 7), frequency = 12) 
+ts_apsTnr <- ts(ts_apsTnr$Measure, start = c(2007, 7), frequency = 12) 
 
 #__________________________________________________________________________________________________#
 
