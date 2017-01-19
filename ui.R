2fe116a6c72c1d64d993d51cc7843ba65ecffcac
Scott Dalwood <ubuxr@prod.atonet.gov.au>
2017-01-19 16:37:23 +1100

    Cleanup ui.R as per Workforce Analytics style guide

--- ui.R
+++ ui.R
@@ -1,55 +1,74 @@
 shinyUI(
+  
   fluidPage(
     
-  # Define link for forecasting explanation
-  link <- "https://en.wikipedia.org/wiki/Exponential_smoothing#Triple_exponential_smoothing",
-    
-  # CSS style for navbar html - added padding-top:50px; to .h2,h2 to accommodate navbar
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")),
-  
-  # Navbar HTML
-  includeHTML("www/navbar.html"),
-    
-  titlePanel("HR Time Series Forecasting"),
-  
-  sidebarLayout(
-    sidebarPanel(selectInput("sel_hrTS", label = ("Choose HR metric to forecast:"),
-                             choices = c("Unplanned Leave (Days per FTE)",
-                                         "Planned Leave (Days per FTE)",
-                                         "Ongoing Workforce (HC)",
-                                         "Non-Ongoing Workforce (HC)",
-                                         "Casual Workforce (HC)",
-                                         "Total Employed Workforce (HC)",
-                                         "Ongoing Workforce (Paid FTE)",
-                                         "Non-Ongoing Workforce (Paid FTE)",
-                                         "Casual Workforce (Paid FTE)",
-                                         "Total Employed Workforce (Paid FTE)",
-                                         "Overall Ongoing Separation Rate (%)",
-                                         "Natural Attrition Separation Rate (%)",
-                                         "Redundancy Separation Rate (%)",
-                                         "Average Age (Years)",
-                                         "Average ATO Tenure (Years)",
-                                         "Average APS Tenure (Years)",
-                                         "Diversity - Non-English Speaking Background (%)",
-                                         "Diversity - Indigenous (%)",
-                                         "Diversity - Disability (%)",
-                                         "Average Female Salary (% of Average Male Salary)"),
-                             selected = "Unplanned Leave (Days per FTE)")
-                 , br()
-                 , sliderInput("fcast_mnths", label = ("Choose number of months to forecast:"),
-                               min = 1, max = 12, step = 1, value = 6)
-                 , tags$a(href = link, "Information on Holt-Winters forecasting", target = "_blank")
-    ),
-    
-    mainPanel(
-      plotlyOutput('forecastPlot')
-      , br()
-      , downloadButton('downloadData', 'Download Data', class = 'btn-primary')
-      , br()
-      , br()
-      , dataTableOutput(outputId="tsTable")
+    # Define link and text for forecasting explanation
+    link <- "https://en.wikipedia.org/wiki/Exponential_smoothing#Triple_exponential_smoothing",
+    text <- "Information on Holt-Winters forecasting",
       
+    # CSS style for navbar html - added padding-top:50px; to .h2,h2 to accommodate navbar
+    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")),
+    
+    # Navbar HTML
+    includeHTML("www/navbar.html"),
+      
+    titlePanel("HR Time Series Forecasting"),
+    
+    sidebarLayout(
+      
+      sidebarPanel(
+        
+        selectInput(inputId    = "sel_hrTS"
+                    , label    = "Choose HR metric to forecast:"
+                    , choices  = c("Unplanned Leave (Days per FTE)"
+                                   , "Planned Leave (Days per FTE)"
+                                   , "Ongoing Workforce (HC)"
+                                   , "Non-Ongoing Workforce (HC)"
+                                   , "Casual Workforce (HC)"
+                                   , "Total Employed Workforce (HC)"
+                                   , "Ongoing Workforce (Paid FTE)"
+                                   , "Non-Ongoing Workforce (Paid FTE)"
+                                   , "Casual Workforce (Paid FTE)"
+                                   , "Total Employed Workforce (Paid FTE)"
+                                   , "Overall Ongoing Separation Rate (%)"
+                                   , "Natural Attrition Separation Rate (%)"
+                                   , "Redundancy Separation Rate (%)"
+                                   , "Average Age (Years)"
+                                   , "Average ATO Tenure (Years)"
+                                   , "Average APS Tenure (Years)"
+                                   , "Diversity - Non-English Speaking Background (%)"
+                                   , "Diversity - Indigenous (%)"
+                                   , "Diversity - Disability (%)"
+                                   , "Average Female Salary (% of Average Male Salary)"),
+                       selected = "Unplanned Leave (Days per FTE)"),
+        
+        br(),
+        
+        
+        sliderInput(inputId = "fcast_mnths"
+                    , label = "Choose number of months to forecast:"
+                    , min   = 1
+                    , max   = 12
+                    , step  = 1
+                    , value = 6),
+        
+        tags$a(href = link, text, target = "_blank")
+        
+      ),
+      
+      mainPanel(
+        
+        plotlyOutput(outputId = "forecastPlot"),
+        br(),
+        downloadButton(outputId = 'downloadData'
+                       , label  = 'Download Data'
+                       , class  = 'btn-primary'),
+        br(),
+        br(),
+        dataTableOutput(outputId = "tsTable")
+        
+      )
+    
     )
-   )
   )
 )
