2972a4416cf0f96747bc38b46f08d7faabb5dca5
Scott Dalwood <scottdalwood@gmail.com>
2017-06-08 14:12:28 +1000

    Change data frame name to match Update script output

--- global.R
+++ global.R
@@ -14,14 +14,14 @@ enableBookmarking(store = "url")
 # LOAD DATA ----------------------------------------------------------------------------------------
 
 # Set path for data files
-dataPath <- "/proj/workforce/data/shinyApps/"
+data_path <- "/proj/workforce/data/shinyApps/"
 
 # Load new big df
-df <- read_csv(paste0(dataPath,"forecastMetrics/TMP_timeSeries.csv"))
+df <- read_csv(paste0(data_path,"forecastMetrics/timeSeries.csv"))
 
 # Separation Rate (%)
-ts_sepn     <- read_csv(paste0(dataPath,"forecastMetrics/forecastSepn.csv"))
-ts_attr     <- read_csv(paste0(dataPath,"forecastMetrics/forecastAttr.csv"))
-ts_rdncy    <- read_csv(paste0(dataPath,"forecastMetrics/forecastRdncy.csv"))
-ts_attrpcnt <- data.frame(Measure=(ts_attr$Measure/ts_sepn$Measure)*100, Sepn_Mth=ts_sepn$Sepn_Mth)
-ts_rdncypcnt<- data.frame(Measure=(ts_rdncy$Measure/ts_sepn$Measure)*100, Sepn_Mth=ts_sepn$Sepn_Mth)
+ts_sepn      <- read_csv(paste0(data_path, "forecastMetrics/forecastSepn.csv"))
+ts_attr      <- read_csv(paste0(data_path, "forecastMetrics/forecastAttr.csv"))
+ts_rdncy     <- read_csv(paste0(data_path, "forecastMetrics/forecastRdncy.csv"))
+ts_attrpcnt  <- data.frame(Measure=(ts_attr$Measure/ts_sepn$Measure)*100, Sepn_Mth=ts_sepn$Sepn_Mth)
+ts_rdncypcnt <- data.frame(Measure=(ts_rdncy$Measure/ts_sepn$Measure)*100, Sepn_Mth=ts_sepn$Sepn_Mth)
